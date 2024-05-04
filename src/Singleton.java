6fb64740c313e63b3c5fd9a4f6deeaacb4b49309
Rxwll <mastpokemon99@gmail.com>
2024-05-04 02:03:01 -0600

    Inicio

---
+++ src/Singleton.java
@@ -0,0 +1,35 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class Singleton {
+    private static Singleton instance;
+    private Connection conn;
+    private Singleton()  {
+        ResourceBundle rb = ResourceBundle.getBundle("Credentials");
+        String url = rb.getString("url");
+        String user = rb.getString("user");
+        String password = rb.getString("password");
+
+        try {
+            this.conn = DriverManager.getConnection(url,user,password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static Singleton getInstance(){
+        if (instance == null){
+            synchronized (Singleton.class){
+                if (instance == null){
+                    instance = new Singleton();
+                }
+            }
+        }
+       return instance;
+    }
+    public Connection getConnection() {
+        return conn;
+    }
+
+}
